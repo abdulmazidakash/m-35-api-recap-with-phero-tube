0611ec8441d34cc2d905f349d277c8eb80c208ff
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-10-03 15:20:18 +0600

    35-12 implement search functionality and module summaury

--- js/test.js
+++ js/test.js
@@ -19,3 +19,19 @@
 // 	return `${hour} hour ${minute} minute ago ${remainingSecond} second ago`;
 // }
 
+
+//ph-tube summary
+/**
+ * 1. load and display data initially
+ * 2. onclick and display data
+ * 3. complex object accessing 
+ * 4. toggle active class functionality
+ * 5. search functionality
+ * 6. load and display single data
+ * 7. display data in a modal
+ * 8. load and display single category data
+ * 9. conditional rendering if array dont have any data
+ * 10. conditional rendering: : if a value empty
+ * 11. conditional rendering:: if a value length is zero
+ */
+
